8a81597c03f668450eafdb1254c73bad883c909f
George Kapris <gkapris@outlook.com>
2023-07-22 18:18:31 +0300

    feat/header button dynamic styling

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ButtonLabels } from '../../constants/ButtonLabels';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  ButtonLabels = ButtonLabels;
+  selectedPath: string = '';
 
+  changeButtonColor(buttonLabel: string): string {
+    if (this.selectedPath == buttonLabel) {
+      return 'primary';
+    }
+    return '';
+  }
 }
