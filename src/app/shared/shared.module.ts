7df40c1108cb84eb8bca03ea59e42a35b56f7f3d
George Kapris <gkapris@outlook.com>
2023-07-22 20:54:24 +0300

    feat: Loader component implementation, interceptor enabler and unit testing

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [CommonModule, MatButtonModule, FlexLayoutModule],
   exports: [HeaderComponent],
+=======
+import { LoaderComponent } from './components/loader/loader.component';
+
+@NgModule({
+  declarations: [HeaderComponent, LoaderComponent],
+  imports: [CommonModule, MatButtonModule, FlexLayoutModule],
+  exports: [HeaderComponent, LoaderComponent],
+>>>>>>> Stashed changes
 })
 export class SharedModule {}
