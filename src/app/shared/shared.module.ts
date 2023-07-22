14559c4fec320c51c9758fac1136583d7946bfc3
George Kapris <gkapris@outlook.com>
2023-07-22 18:09:41 +0300

    feat/template creation and styling

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, FlexLayoutModule],
   exports: [HeaderComponent],
 })
 export class SharedModule {}
