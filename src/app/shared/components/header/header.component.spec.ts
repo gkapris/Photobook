c22fd9936dc835d7dec19d9bf39859d983e472f0
George Kapris <gkapris@outlook.com>
2023-07-22 18:50:59 +0300

    feat/unit testing header component - draft

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { ButtonLabels } from '../../constants/ButtonLabels';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +10,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [HeaderComponent],
+      imports: [MatButtonModule],
     });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -17,5 +20,19 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#changeButtonColor', () => {
+    it('should return primary when selected path is equal with current path (draft)', () => {
+      component.selectedPath = ButtonLabels.PHOTOS;
+      const returnedValue = component.changeButtonColor(ButtonLabels.PHOTOS);
+      expect(returnedValue).toEqual('primary');
+    });
+
+    it('should return blank when selected path is NOT equal with current path (draft)', () => {
+      component.selectedPath = '';
+      const returnedValue = component.changeButtonColor(ButtonLabels.PHOTOS);
+      expect(returnedValue).toEqual('');
+    });
   });
 });
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -9,6 +9,8 @@ import { ButtonLabels } from '../../constants/ButtonLabels';
 export class HeaderComponent {
   ButtonLabels = ButtonLabels;
   selectedPath: string = '';
+  // selectedPath: string = ButtonLabels.FAVORITES;
+  // selectedPath: string = ButtonLabels.PHOTOS;
 
   changeButtonColor(buttonLabel: string): string {
     if (this.selectedPath == buttonLabel) {
