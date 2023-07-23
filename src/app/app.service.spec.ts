1269361d391fcb364c33521113630038dd02a33d
George Kapris <gkapris@outlook.com>
2023-07-23 16:04:50 +0300

    feat save photo ids to show in favorites page

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -9,8 +9,8 @@ import {
   IPexelsListResponse,
   IPexelsPhoto,
 } from './shared/interfaces/IPexelsPhoto.interface';
-import { HttpClient, HttpParamsOptions } from '@angular/common/http';
-import { PexelsURLS } from './shared/constants/PexelsURLS.enum';
+import { HttpClient } from '@angular/common/http';
+import { PexelsURLSEnum } from './shared/constants/URLS_Enums';
 
 describe('AppService', () => {
   let service: AppService;
@@ -39,64 +39,95 @@ describe('AppService', () => {
     httpMock.verify();
   });
 
-  // Incomplete unit test due time shortage
-  it('should fetch and update photo list periodically', () => {
-    jest.useFakeTimers();
+  describe.skip('#getPhotoList', () => {
+    // Incomplete unit test due time shortage
+    it('should fetch and update photo list periodically', () => {
+      jest.useFakeTimers();
 
-    const photosListResponse: IPexelsListResponse = {
-      total_results: 4,
-      page: 1,
-      per_page: 4,
-      photos: [
-        { id: 1, url: 'url1' },
-        { id: 2, url: 'url2' },
-        { id: 3, url: 'url3' },
-        { id: 4, url: 'url4' },
-      ],
-    } as IPexelsListResponse;
-    const mockHttpClient = TestBed.inject(HttpClient);
-    jest.spyOn(mockHttpClient, 'get').mockReturnValue(of(photosListResponse));
+      const photosListResponse: IPexelsListResponse = {
+        total_results: 4,
+        page: 1,
+        per_page: 4,
+        photos: [
+          { id: 1, url: 'url1' },
+          { id: 2, url: 'url2' },
+          { id: 3, url: 'url3' },
+          { id: 4, url: 'url4' },
+        ],
+      } as IPexelsListResponse;
+      const mockHttpClient = TestBed.inject(HttpClient);
+      jest.spyOn(mockHttpClient, 'get').mockReturnValue(of(photosListResponse));
 
-    service.getPhotoList();
+      service.getPhotoList();
 
-    expect(mockPhotosList$.getValue()).toEqual([]);
+      expect(mockPhotosList$.getValue()).toEqual([]);
 
-    jest.advanceTimersByTime(10000);
+      jest.advanceTimersByTime(10000);
 
-    expect(mockHttpClient.get).toHaveBeenCalledTimes(1);
-    // expect(mockHttpClient.get).toHaveBeenCalledWith(
-    //   PexelsURLS.GetPhotosListByCategory
-    // );
+      expect(mockHttpClient.get).toHaveBeenCalledTimes(1);
+      // expect(mockHttpClient.get).toHaveBeenCalledWith(
+      //   PexelsURLSEnum.GetPhotosListByCategory
+      // );
 
-    jest.advanceTimersByTime(10000);
+      jest.advanceTimersByTime(10000);
 
-    expect(mockHttpClient.get).toHaveBeenCalledTimes(2);
+      expect(mockHttpClient.get).toHaveBeenCalledTimes(2);
 
-    const photosListResponse2: IPexelsListResponse = {
-      ...photosListResponse,
-      page: 2,
-      photos: [
-        { id: 5, url: 'url5' },
-        { id: 6, url: 'url6' },
-        { id: 7, url: 'url7' },
-        { id: 8, url: 'url8' },
-      ],
-    } as IPexelsListResponse;
-    jest.spyOn(mockHttpClient, 'get').mockReturnValue(of(photosListResponse2));
+      const photosListResponse2: IPexelsListResponse = {
+        ...photosListResponse,
+        page: 2,
+        photos: [
+          { id: 5, url: 'url5' },
+          { id: 6, url: 'url6' },
+          { id: 7, url: 'url7' },
+          { id: 8, url: 'url8' },
+        ],
+      } as IPexelsListResponse;
+      jest
+        .spyOn(mockHttpClient, 'get')
+        .mockReturnValue(of(photosListResponse2));
 
-    jest.advanceTimersByTime(1000);
+      jest.advanceTimersByTime(1000);
 
-    // expect(mockHttpClient.get).toHaveBeenCalledWith(
-    //   PexelsURLS.GetPhotosListByCategory,
-    //   { params: { page: 2 } }
-    // );
+      // expect(mockHttpClient.get).toHaveBeenCalledWith(
+      //   PexelsURLSEnum.GetPhotosListByCategory,
+      //   { params: { page: 2 } }
+      // );
 
-    expect(mockPhotosList$.getValue()).toEqual([
-      // ...photosListResponse.photos,
-      // ...photosListResponse2.photos,
-    ]);
+      expect(mockPhotosList$.getValue()).toEqual([
+        // ...photosListResponse.photos,
+        // ...photosListResponse2.photos,
+      ]);
 
-    // Cleanup fake timers
-    jest.useRealTimers();
+      // Cleanup fake timers
+      jest.useRealTimers();
+    });
+  });
+
+  describe('#saveFavoritePhot', () => {
+    it('should add new ids to the services property', () => {
+      const testingValue = 1223221;
+      service.saveFavoritePhoto(testingValue);
+      const savedValues = service.getFavoritePhotosList();
+      expect(savedValues).toContain(testingValue);
+    });
+  });
+
+  describe('#getFavoritePhoto', () => {
+    it('should fetch a favorite photo by ID', () => {
+      const mockPhotoId = 123;
+      const mockPhotoData: IPexelsPhoto = {} as IPexelsPhoto;
+
+      service.getFavoritePhoto(mockPhotoId).subscribe((photo: IPexelsPhoto) => {
+        expect(photo).toBeTruthy();
+        expect(photo).toEqual(mockPhotoData);
+      });
+
+      const request = httpMock.expectOne(
+        `${PexelsURLSEnum.GetPhotoById}?photos=${mockPhotoId}`
+      );
+      expect(request.request.method).toBe('GET');
+      request.flush(mockPhotoData);
+    });
   });
 });
--- src/app/shared/constants/URLS_Enums.ts
+++ src/app/shared/constants/URLS_Enums.ts
@@ -1,4 +1,8 @@
-export enum PexelsURLS {
+export enum PexelsURLSEnum {
   GetPhotosListByCategory = 'https://api.pexels.com/v1/search',
   GetPhotoById = 'https://api.pexels.com/v1/photos/',
+}
+
+export enum PhotosJSONEnum {
+  PATH = './../../../assets/favorites.json',
 }
