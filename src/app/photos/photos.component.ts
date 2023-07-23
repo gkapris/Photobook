3dbad506715c0fe680adf080d1b61a20c935dfcc
George Kapris <gkapris@outlook.com>
2023-07-23 13:06:09 +0300

    fix set photos to component, add unit testing (#7)

--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -8,15 +8,12 @@ import { IPexelsPhoto } from '../shared/interfaces/IPexelsPhoto.interface';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.scss'],
 })
-export class PhotosComponent implements OnInit, AfterViewInit {
-  photos: Observable<IPexelsPhoto[]> = new Observable();
+export class PhotosComponent implements OnInit {
+  photos: Observable<IPexelsPhoto[]> = new Observable<IPexelsPhoto[]>();
   constructor(private readonly appService: AppService) {}
 
   ngOnInit(): void {
     this.appService.getPhotoList();
-  }
-
-  ngAfterViewInit(): void {
     this.photos = this.appService.photosList$.asObservable();
   }
 
