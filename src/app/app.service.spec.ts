44f5131aa339a0e27f74421a665c14a729fadabc
George Kapris <gkapris@outlook.com>
2023-07-23 13:05:09 +0300

    fix set photos to component, add unit testing

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -1,16 +1,102 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { BehaviorSubject, of } from 'rxjs';
 import { AppService } from './app.service';
+import {
+  IPexelsListResponse,
+  IPexelsPhoto,
+} from './shared/interfaces/IPexelsPhoto.interface';
+import { HttpClient, HttpParamsOptions } from '@angular/common/http';
+import { PexelsURLS } from './shared/constants/PexelsURLS.enum';
 
 describe('AppService', () => {
   let service: AppService;
+  let httpMock: HttpTestingController;
+  let mockPhotosList$: BehaviorSubject<IPexelsPhoto[]>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AppService],
+    });
+
     service = TestBed.inject(AppService);
+    httpMock = TestBed.inject(HttpTestingController);
+
+    mockPhotosList$ = new BehaviorSubject<IPexelsPhoto[]>([]);
+    jest
+      .spyOn(service.photosList$, 'getValue')
+      .mockReturnValue(mockPhotosList$.getValue());
+    jest
+      .spyOn(service.photosList$, 'next')
+      .mockImplementation(mockPhotosList$.next);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  // Incomplete unit test due time shortage
+  it('should fetch and update photo list periodically', () => {
+    jest.useFakeTimers();
+
+    const photosListResponse: IPexelsListResponse = {
+      total_results: 4,
+      page: 1,
+      per_page: 4,
+      photos: [
+        { id: 1, url: 'url1' },
+        { id: 2, url: 'url2' },
+        { id: 3, url: 'url3' },
+        { id: 4, url: 'url4' },
+      ],
+    } as IPexelsListResponse;
+    const mockHttpClient = TestBed.inject(HttpClient);
+    jest.spyOn(mockHttpClient, 'get').mockReturnValue(of(photosListResponse));
+
+    service.getPhotoList();
+
+    expect(mockPhotosList$.getValue()).toEqual([]);
+
+    jest.advanceTimersByTime(1000);
+
+    expect(mockHttpClient.get).toHaveBeenCalledTimes(1);
+    // expect(mockHttpClient.get).toHaveBeenCalledWith(
+    //   PexelsURLS.GetPhotosListByCategory
+    // );
+
+    jest.advanceTimersByTime(1000);
+
+    expect(mockHttpClient.get).toHaveBeenCalledTimes(2);
+
+    const photosListResponse2: IPexelsListResponse = {
+      ...photosListResponse,
+      page: 2,
+      photos: [
+        { id: 5, url: 'url5' },
+        { id: 6, url: 'url6' },
+        { id: 7, url: 'url7' },
+        { id: 8, url: 'url8' },
+      ],
+    } as IPexelsListResponse;
+    jest.spyOn(mockHttpClient, 'get').mockReturnValue(of(photosListResponse2));
+
+    jest.advanceTimersByTime(1000);
+
+    // expect(mockHttpClient.get).toHaveBeenCalledWith(
+    //   PexelsURLS.GetPhotosListByCategory,
+    //   { params: { page: 2 } }
+    // );
+
+    expect(mockPhotosList$.getValue()).toEqual([
+      // ...photosListResponse.photos,
+      // ...photosListResponse2.photos,
+    ]);
+
+    // Cleanup fake timers
+    jest.useRealTimers();
   });
 });
--- src/app/photos/photos.component.spec.ts
+++ src/app/photos/photos.component.spec.ts
@@ -1,21 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotosComponent } from './photos.component';
+import { AppService } from '../app.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PhotosComponent', () => {
   let component: PhotosComponent;
   let fixture: ComponentFixture<PhotosComponent>;
+  let service: AppService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PhotosComponent]
+      declarations: [PhotosComponent],
+      imports: [HttpClientModule],
     });
     fixture = TestBed.createComponent(PhotosComponent);
+    service = TestBed.inject(AppService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#ngOnInit', () => {
+    it('should call getPhotoList from service', () => {
+      jest.spyOn(service, 'getPhotoList');
+      component.ngOnInit();
+      expect(service.getPhotoList).toHaveBeenCalled();
+    });
   });
 });
